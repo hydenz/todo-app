32654a642d467833263d48ea4a1f1ab64981ab1e
Marcelo F. de Lima <marcelof_lima@hotmail.com>
2022-10-12 10:36:49 -0300

    refactor: remove unused import

--- src/components/List/List.component.tsx
+++ src/components/List/List.component.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading  */
-import { cloneElement, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   DragDropContext,
   Droppable,
